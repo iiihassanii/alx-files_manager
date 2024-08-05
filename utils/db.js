f39ffc7cb2f6738f6575aacbfde055f4f0e64d88
iiihassanii <hassanabdelazim.max@gmail.com>
2024-08-05 15:26:13 +0300

    task 2

--- utils/db.js
+++ utils/db.js
@@ -5,36 +5,32 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 class DBClient {
-    constructor() {
-        const host = process.env.DB_HOST || 'localhost';
-        const port = process.env.DB_PORT || 27017;
-        const database = process.env.DB_DATABASE || 'files_manager';
-        const url = `mongodb://${host}:${port}`;
+  constructor() {
+      const host = process.env.DB_HOST || 'localhost';
+      const port = process.env.DB_PORT || 27017;
+      const database = process.env.DB_DATABASE || 'files_manager';
+      const url = `mongodb://${host}:${port}`;
 
-        this.client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
-        this.client.connect()
-            .then(() => {
-                this.db = this.client.db(database);
-                console.log('MongoDB client connected to the server');
-            })
-            .catch((err) => {
-                console.error('MongoDB client not connected to the server:', err);
-            });
-    }
+      this.client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+      this.client.connect()
+          .then(() => {
+              this.db = this.client.db(database);
+          })
+  }
 
-    isAlive() {
-        return this.client && this.client.isConnected();
-    }
+  isAlive() {
+      return this.client && this.client.isConnected();
+  }
 
-    async nbUsers() {
-        if (!this.db) return 0;
-        return this.db.collection('users').countDocuments();
-    }
+  async nbUsers() {
+      if (!this.db) return 0;
+      return this.db.collection('users').countDocuments();
+  }
 
-    async nbFiles() {
-        if (!this.db) return 0;
-        return this.db.collection('files').countDocuments();
-    }
+  async nbFiles() {
+      if (!this.db) return 0;
+      return this.db.collection('files').countDocuments();
+  }
 }
 
 const dbClient = new DBClient();
