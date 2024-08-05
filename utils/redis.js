64135d6355ca3c55151e985ad077cf31c701fb99
iiihassanii <hassanabdelazim.max@gmail.com>
2024-08-05 14:56:27 +0300

    tasl 0

--- utils/redis.js
+++ utils/redis.js
@@ -2,43 +2,42 @@
 import { createClient } from 'redis';
 
 class RedisClient {
-    constructor() {
-        this.client = createClient();
-        this.client.on('error', (err) => {
-            console.error('client not connected to the server:', err);
-        });
-    }
-    isAlive() {
-        return this.client.connected;
-    }
+  constructor() {
+      this.client = createClient();
+      this.client.on('error', (err) => {
+          console.error('client not connected to the server:', err);
+      });
+  }
+  isAlive() {
+      return this.client.connected;
+  }
 
-    async get(key) {
-        return new Promise((resolve, reject) => {
-            this.client.get(key, (err, value) => {
-                resolve(value);
-            });
-        });
-    }
+  async get(key) {
+      return new Promise((resolve, reject) => {
+          this.client.get(key, (err, value) => {
+              resolve(value);
+          });
+      });
+  }
 
-    async set(key, value, duration) {
-        return new Promise((resolve, reject) => {
-            this.client.set(key, value, 'EX', duration)
-                resolve();
-            });
-     
-    }
+  async set(key, value, duration) {
+      return new Promise((resolve, reject) => {
+          this.client.set(key, value, 'EX', duration)
+              resolve();
+          });
+  }
 
-    async del(key) {
-        return new Promise((resolve, reject) => {
-            this.client.del(key, (err) => {
-                if (err) {
-                    console.error('Error deleting key from Redis:', err);
-                    return reject(err);
-                }
-                resolve();
-            });
-        });
-    }
+  async del(key) {
+      return new Promise((resolve, reject) => {
+          this.client.del(key, (err) => {
+              if (err) {
+                  console.error('Error deleting key from Redis:', err);
+                  return reject(err);
+              }
+              resolve();
+          });
+      });
+  }
 }
 
 const redisClient = new RedisClient();
