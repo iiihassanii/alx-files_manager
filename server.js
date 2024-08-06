cddd2203332b7c2237766f63f57100ca918d4040
iiihassanii <hassanabdelazim.max@gmail.com>
2024-08-06 14:09:32 +0300

    task 3

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
+// server.js
 import express from 'express';
 import dotenv from 'dotenv';
 import routes from './routes/index.js';
 
 dotenv.config();
 
-const app = express()
+const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(express.json());
 app.use('/', routes);
 
 app.listen(port, () => {
