85b027431901ce80b08f449387bfa1470b25ed64
iiihassanii <hassanabdelazim.max@gmail.com>
2024-08-06 13:21:17 +0300

    task 2

--- utils/db.js
+++ utils/db.js
@@ -4,32 +4,50 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+/**
+ * Class representing a MongoDB client.
+ */
 class DBClient {
+  /**
+   * Creates an instance of DBClient and initializes the MongoDB client.
+   */
   constructor() {
-      const host = process.env.DB_HOST || 'localhost';
-      const port = process.env.DB_PORT || 27017;
-      const database = process.env.DB_DATABASE || 'files_manager';
-      const url = `mongodb://${host}:${port}`;
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const url = `mongodb://${host}:${port}`;
 
-      this.client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
-      this.client.connect()
-          .then(() => {
-              this.db = this.client.db(database);
-          })
+    this.client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+    this.client.connect()
+      .then(() => {
+        this.db = this.client.db(database);
+      });
   }
 
+  /**
+   * Checks if the MongoDB client is connected.
+   * @returns {boolean} - True if the client is connected, false otherwise.
+   */
   isAlive() {
-      return this.client && this.client.isConnected();
+    return this.client && this.client.isConnected();
   }
 
+  /**
+   * Retrieves the number of users in the 'users' collection.
+   * @returns {Promise<number>} - A promise that resolves to the number of users.
+   */
   async nbUsers() {
-      if (!this.db) return 0;
-      return this.db.collection('users').countDocuments();
+    if (!this.db) return 0;
+    return this.db.collection('users').countDocuments();
   }
 
+  /**
+   * Retrieves the number of files in the 'files' collection.
+   * @returns {Promise<number>} - A promise that resolves to the number of files.
+   */
   async nbFiles() {
-      if (!this.db) return 0;
-      return this.db.collection('files').countDocuments();
+    if (!this.db) return 0;
+    return this.db.collection('files').countDocuments();
   }
 }
 
--- utils/redis.js
+++ utils/redis.js
@@ -1,32 +1,69 @@
 // utils/redis.js
 import { createClient } from 'redis';
 
+/**
+ * Class representing a Redis client.
+ */
 class RedisClient {
+  /**
+    * Creates an instance of RedisClient and initializes the Redis client.
+   */
   constructor() {
       this.client = createClient();
       this.client.on('error', (err) => {
           console.error('client not connected to the server:', err);
       });
   }
+
+  /**
+    * Checks if the Redis client is connected.
+    * @returns {boolean} - True if the client is connected, false otherwise.
+    */
   isAlive() {
       return this.client.connected;
   }
 
+  /**
+    * Retrieves the value associated with the specified key from Redis.
+    * @param {string} key - The key to retrieve the value for.
+    * @returns {Promise<string|null>} - A promise that resolves to the value of the key, or null if the key does not exist.
+    */
   async get(key) {
       return new Promise((resolve, reject) => {
           this.client.get(key, (err, value) => {
+              if (err) {
+                  console.error('Error getting value from Redis:', err);
+                  return reject(err);
+              }
               resolve(value);
           });
       });
   }
 
+  /**
+    * Sets a key-value pair in Redis with an expiration time.
+    * @param {string} key - The key to set.
+    * @param {string} value - The value to set.
+    * @param {number} duration - The expiration time in seconds.
+    * @returns {Promise<void>} - A promise that resolves when the key-value pair is set.
+    */
   async set(key, value, duration) {
       return new Promise((resolve, reject) => {
-          this.client.set(key, value, 'EX', duration)
+          this.client.set(key, value, 'EX', duration, (err) => {
+              if (err) {
+                  console.error('Error setting value in Redis:', err);
+                  return reject(err);
+              }
               resolve();
           });
+      });
   }
 
+  /**
+    * Deletes the key-value pair associated with the specified key from Redis.
+   * @param {string} key - The key to delete.
+   * @returns {Promise<void>} - A promise that resolves when the key is deleted.
+    */
   async del(key) {
       return new Promise((resolve, reject) => {
           this.client.del(key, (err) => {
