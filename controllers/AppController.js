db19e3a725e2acfb790853fd066a41f95383884a
Islam Ahmed <eslamelbltagye1111@gmail.com>
2024-08-07 14:36:49 +0300

    a

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,6 +1,6 @@
 // controllers/AppController.js
-import redisClient from '../utils/redis.js';
-import dbClient from '../utils/db.js';
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
 
 class AppController {
   static getStatus(req, res) {
